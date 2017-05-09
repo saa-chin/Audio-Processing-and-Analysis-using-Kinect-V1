271f4c4a5af64cafe5172a5bb8e7b054aca924af
Sachin Hiriyanna <sachinvasista@gmail.com>
2017-05-09 15:23:40 -0400

    cleaned

--- MicArray/MainWindow.xaml.cs
+++ MicArray/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Microsoft.Kinect;
 using System.Windows.Threading;
 using System.IO;
@@ -171,8 +164,6 @@ namespace MicArray
 
         void beamAngleChanged(object sender, BeamAngleChangedEventArgs e)
         {
-            //this.sensor.AudioSource.BeamAngleMode = BeamAngleMode.Manual;
-            //this.sensor.AudioSource.ManualBeamAngle = 30;
 
             this.beam_angle.Text = e.Angle.ToString();
 
